f202fd0069286ec2d87da430f7292c52024b5078
Shreevari SP <shreevari.sp@hashedin.com>
2020-05-20 16:51:48 +0530

    Print image path when corrupt input is encountered

--- encode_faces.py
+++ encode_faces.py
@@ -29,30 +29,37 @@ knownNames = []
 
 # loop over the image paths
 for (i, imagePath) in enumerate(imagePaths):
-	# extract the person name from the image path
-	print("[INFO] processing image {}/{}".format(i + 1,
-		len(imagePaths)))
-	name = imagePath.split(os.path.sep)[-2]
+        # extract the person name from the image path
+        print("[INFO] processing image {}/{}".format(i + 1,
+                len(imagePaths)))
+        name = imagePath.split(os.path.sep)[-2]
 
-	# load the input image and convert it from RGB (OpenCV ordering)
-	# to dlib ordering (RGB)
-	image = cv2.imread(imagePath)
-	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # We intend to skip incompatible images
+        try:
+                # load the input image and convert it from RGB (OpenCV ordering)
+                # to dlib ordering (RGB)
+                image = cv2.imread(imagePath)
+                rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-	# detect the (x, y)-coordinates of the bounding boxes
-	# corresponding to each face in the input image
-	boxes = face_recognition.face_locations(rgb,
-		model=args["detection_method"])
+                # Enable this to test if the skip works
+                #raise ValueError("This is a dummy error")
 
-	# compute the facial embedding for the face
-	encodings = face_recognition.face_encodings(rgb, boxes)
+                # detect the (x, y)-coordinates of the bounding boxes
+                # corresponding to each face in the input image
+                boxes = face_recognition.face_locations(rgb,
+                                                model=args["detection_method"])
 
-	# loop over the encodings
-	for encoding in encodings:
-		# add each encoding + name to our set of known names and
-		# encodings
-		knownEncodings.append(encoding)
-		knownNames.append(name)
+                # compute the facial embedding for the face
+                encodings = face_recognition.face_encodings(rgb, boxes)
+
+                # loop over the encodings
+                for encoding in encodings:
+                        # add each encoding + name to our set of known names and
+                        # encodings
+                        knownEncodings.append(encoding)
+                        knownNames.append(name)
+        except:
+              print(imagePath)  
 
 # dump the facial encodings + names to disk
 print("[INFO] serializing encodings...")
